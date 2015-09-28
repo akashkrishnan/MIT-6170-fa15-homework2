db31a582c71e41cd98b671c946c117b7509ded76
Akash Krishnan <ak@aakay.net>
2015-09-27 20:12:13 -0400

    added reset simulation button; started update logic

--- GameOfLife.js
+++ GameOfLife.js
@@ -9,8 +9,33 @@ var GameOfLife = function ( board ) {
 
   var that = Object.create( GameOfLife );
 
-  // Initialize board
-  that.board = board;
+  var population = board.getPopulation();
+  var simulationInterval;
+
+  var simulator = function () {
+    console.log( 'next' );
+  };
+
+  that.startSimulation = function () {
+    if ( simulationInterval ) {
+      clearInterval( simulationInterval );
+    }
+    population = board.getPopulation();
+    board.setSimulating( true );
+    simulationInterval = setInterval( simulator, 500 );
+  };
+
+  that.pauseSimulation = function () {
+    if ( simulationInterval ) {
+      clearInterval( simulationInterval );
+    }
+    board.setSimulating( false );
+  };
+
+  that.resetSimulation = function () {
+    that.pauseSimulation();
+    board.setPopulation( population );
+  };
 
   Object.freeze( that );
   return that;
